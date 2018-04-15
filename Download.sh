98d3d62c4ebec33d36a4bea0700a5c96ed0d74f9
Harsh Lathwal <xeon.harsh@gmail.com>
2018-04-15 13:54:46 +0530

    added ubuntu 64bit 16.04 download link

--- Download.sh
+++ Download.sh
@@ -1,2 +1,3 @@
-# 64bit link ubuntu 16.04
-wget http://releases.ubuntu.com/16.04.4/ubuntu-16.04.4-desktop-amd64.iso
+#!/usr/bin/env bash
+#64bit link ubuntu 16.04
+wget --quite http://releases.ubuntu.com/16.04.4/ubuntu-16.04.4-desktop-amd64.iso
