8e1c2d876ee5be28ba8ee4433c1e0edad96ff4f4
Harsh Lathwal <xeon.harsh@gmail.com>
2018-04-15 13:58:59 +0530

    quite tyo

--- Download.sh
+++ Download.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #64bit link ubuntu 16.04
-wget --quite http://releases.ubuntu.com/16.04.4/ubuntu-16.04.4-desktop-amd64.iso
+wget --quiet http://releases.ubuntu.com/16.04.4/ubuntu-16.04.4-desktop-amd64.iso
